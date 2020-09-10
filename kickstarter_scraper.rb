650c221b51cd9dc73f0cc538f1bf650135711b99
Sara A <calisaraalhindi@gmail.com>
2020-09-10 01:28:03 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,9 @@
+require "nokogiri" 
+require "fixtures/kickstarter.html"
 # require libraries/modules here
 
 def create_project_hash
+  html= File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
   # write your code here
 end
